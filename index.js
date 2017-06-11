a1fcdb709d21c4dbf0ec2031d0b3b73e5db932dd
joel <joelbitar@protonmail.com>
2017-06-10 20:19:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+$(function() {
+	$('#searchRepos').click(function() {
+		searchRepositories()
+	})
 
-var renderCommit = (commit) => {
-  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
-}
+	function searchRepositories() {
+		var searchTerms = $('#searchTerms').val()
+		var url = `https://api.github.com/search/repositories?q=${searchTerms}`
 
-var renderCommits = (data) => {
-  let result = data.map((commit)=>renderCommit(commit)).join('')
-  return `<ul>${result}</ul>`
-}
+		// AJAX
+		$.get(url)
+	  .done(function(data) {
+		  displayRepositories(data)
+	  });
+	}
 
-var showCommits = (el) => {
-  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
-  }).fail(error => {
-    displayError()
-  })
-}
+	function displayRepositories(data) {
+		var repositoryTemplate = $('#repository-template').html()
+	  var template = Handlebars.compile(repositoryTemplate)
+	  $('#results').html(template(data))
+	  $('#details').empty()
+	  $('.commit-link').click(function() {
+			showCommits(this)
+		})
+	}
 
-var renderSearchResult = (result) => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description}</p>
-      </div>
-      <hr>
-    `
-}
+	function showCommits(e) {
+		// GET /repos/:owner/:repo/commits
+		var repo = e.dataset.repo
+		var owner = e.dataset.owner
+		var url = `https://api.github.com/reos/${owner}/${repo}/commits`
 
-var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+		// AJAX
+		$.get(url)
+	  .done(function(data) {
+		  var commitTemplate = $('#commit-template').html()
+			var template = Handlebars.compile(commitTemplate)
+			$('#details').html(template(data))
+	  })
+	  .fail(function(error) {
+	  	displayError(error)
+		});
+	}
 
-var searchRepositories = () => {
-  const searchTerms = $('#searchTerms').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      $('#results').html(renderSearchResults(data))
-    }).fail(error => {
-      displayError()
-    })
-}
+	function displayError(error) {
+		var errorTemplate = $('#error-template').html()
+		var template = Handlebars.compile(errorTemplate)
+		$('#errors').html(template(error))
+	}
 
-$(document).ready(function (){
 });
