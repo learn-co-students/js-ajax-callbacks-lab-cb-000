931d76e71a373005a6e0a9c32346fdf6962fbeb1
Alex Segers <a.segers.dev@gmail.com>
2018-03-19 03:14:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function searchRepositories() {
+/* index.js */
+function searchRepositories(event) { // include the event as parameter
+  event.preventDefault(); // prevent the page from refreshing after submitting the form
   var searchTerms = $('#searchTerms').val();
   var url = `https://api.github.com/search/repositories?q=${searchTerms}`;
   $.get(url, function(data) {
-    displayRepositories()
+    displayRepositories(data) // give the displayRepositories() the data from the API
   }).fail(function(error) {
     displayError()
   })
@@ -11,16 +13,16 @@ function searchRepositories() {
 
 
 function displayError() {
-  $('div#errors').html("<p>I'm sorry, there's been an error. Please try again.</p>")
+  $('div#errors').innerHTML = "<p>I'm sorry, there's been an error. Please try again.</p>"
 }
 
 
-function displayRepositories() {
+function displayRepositories(data) { // get the data from the API
   var templateFn = Handlebars.compile(document.getElementById("repositories-template").innerHTML);
-  var repos = templateFn(JSON.parse(this));
-  alert(repos)
+  var repos = templateFn(data); // the data is already a JSON object, no need to parse it again
+  // alert(repos)
   document.getElementById('results').innerHTML = repos;
-  alert(document.getElementById('results').innerHTML)
+  // alert(document.getElementById('results').innerHTML)
 }
 
 // function searchCommits() {}
