17fc5a356c682ffe7f040a89d3f78c7d8882823b
Heather Christman <efl7a@yahoo.de>
2018-03-24 09:55:35 -0400

    added handlebars so that I can see the layout of the results better.

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function searchRepositories() {
   var searchTerms = document.getElementById('searchTerms').value
   var url = "https://api.github.com/search/repositories?q=" + searchTerms
   $.get(url, function(data){
-    var list = `<ul>${data.items.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a> -' + r.owner.login + '- <a href="#" data-repository="'+ r.name + '" data-owner="' + r.owner.login + '" onclick="showCommits(this)">Get Commits</a></li>').join('')}</ul>`
-    $("#results").html(list)
-  }).fail(displayError(error));
+    var src = document.getElementById("repo-template").innerHTML
+    const template = Handlebars.compile(src)
+    const repoList = template(data.items)
+    $("#results").html(repoList)
+  }).fail(error => displayError());
 }
 function displayError(error) {
   $("#errors").html("I'm sorry, there's been an error. Please try again")
@@ -14,9 +16,11 @@ function displayError(error) {
 
 function showCommits(el) {
   var url ="https://api.github.com/repos/"+ el.dataset.owner + "/"+ el.dataset.repository + "/commits"
-  console.log(url)
   $.get(url, function(data) {
-    var list = `<ul>${data.map(item => '<li>' + item.commit.author.name + '<br>' + item.sha + '<img src="' + item.committer.avatar_url + '" width="32" height= "25"></li>').join('')}</ul>`
-    $("#details").html(list)
+    var src = document.getElementById("commits-template").innerHTML
+    const template = Handlebars.compile(src)
+    const commitsList = template(data)
+//    var list = `<ul>${data.map(item => '<li>' + item.commit.author.name + '<br>' + item.sha + '<img src="' + item.committer.avatar_url + '" width="32" height= "25"></li>').join('')}</ul>`
+    $("#details").html(commitsList)
   });
 }
