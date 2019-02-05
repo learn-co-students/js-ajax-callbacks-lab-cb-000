ad45a26da50509afa39564b245f9f69ea3edd9f4
dapawn <dapawn@gmail.com>
2019-02-04 20:01:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
-$(document).ready(function (){
-});
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+var renderCommits = (data) => {
+  let result = data.map(
+    (commit) => {
+      return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+    }
+  ).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError();
+  })
+}
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const terms = document.getElementById("searchTerms").value;
+  $.get(`https://api.github.com/search/repositories?q=${terms}`, data => { 
+    $("#results").html(renderSearchResults(data))
+  }).fail(err => {
+    displayError(err);
+  });
+}
+
+$(document).ready(()=>{});
