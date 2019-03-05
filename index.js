a8bf0a9edcf674e8b8395cf002b9f07534b331cb
Chris Franklin <cf73021@gmail.com>
2019-03-05 02:49:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
 $(document).ready(function (){
 });
-
-function searchRepositories(){
-  let query = document.getElementById("searchTerms").value
-  const apiQuery = 'https://api.github.com/search/repositories?q=' + query
-  $.get(apiQuery, function (response) {
-      let resultsArr = []
-      for (const item of response.items) {
-          let projectHash = {}
-          projectHash.repository = item.name
-          projectHash.description = item.description
-          projectHash.link = item.html_url
-          projectHash.avatar = item.owner.avatar_url
-          projectHash.owner = item.owner.login
-          projectHash.profile = item.owner.html_url
-          projectHash.commitsUrl = item.commits_url;
-          projectHash.gitCommitsUrl = item.git_commits_url;
-          resultsArr.push(projectHash)
-       }
-      displayData(resultsArr)
-  }).fail(function () {
-      displayError()
-  });
-}
