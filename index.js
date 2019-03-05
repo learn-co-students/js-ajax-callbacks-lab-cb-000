3f4cea111a6a3926971488bc5fded6270781e06d
Chris Franklin <cf73021@gmail.com>
2019-03-05 02:47:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+  let query = document.getElementById("searchTerms").value
+  const apiQuery = 'https://api.github.com/search/repositories?q=' + query
+  $.get(apiQuery, function (response) {
+      let resultsArr = []
+      for (const item of response.items) {
+          let projectHash = {}
+          projectHash.repository = item.name
+          projectHash.description = item.description
+          projectHash.link = item.html_url
+          projectHash.avatar = item.owner.avatar_url
+          projectHash.owner = item.owner.login
+          projectHash.profile = item.owner.html_url
+          projectHash.commitsUrl = item.commits_url;
+          projectHash.gitCommitsUrl = item.git_commits_url;
+          resultsArr.push(projectHash)
+       }
+      displayData(resultsArr)
+  }).fail(function () {
+      displayError()
+  });
+}
